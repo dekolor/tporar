f1454f655723d5e1228a323486df280064fc43f1
stefan <25616391+dekolor@users.noreply.github.com>
2022-11-15 21:38:32 +0200

    remove junk

--- src/components/BusRoutes.js
+++ src/components/BusRoutes.js
@@ -9,51 +9,6 @@ function BusRoutes(props) {
   }, [linii, props.linii]);
 
   const displayLines = linii.map((linie) => {
-    // if (
-    //   Number(linie.name.replace(/\D/g, "")) > 399 &&
-    //   Number(linie.name.replace(/\D/g, "")) < 500
-    // ) {
-    //   //regionale
-    //   return (
-    //     <button key={linie.id} className="btn btn-info badgelinie">
-    //       {linie.name}
-    //     </button>
-    //   );
-    // } else if (linie.name[0] == "N") {
-    //   //linii de noapte
-    //   return (
-    //     <button key={linie.id} className="btn btn-dark badgelinie">
-    //       {linie.name}
-    //     </button>
-    //   );
-    // } else if (
-    //   Number(linie.name.replace(/\D/g, "")) > 599 &&
-    //   Number(linie.name.replace(/\D/g, "")) < 700
-    // ) {
-    //   //temporare
-    //   return (
-    //     <button key={linie.id} className="btn btn-danger badgelinie">
-    //       {linie.name}
-    //     </button>
-    //   );
-    // } else if (
-    //   Number(linie.name.replace(/\D/g, "")) > 699 &&
-    //   Number(linie.name.replace(/\D/g, "")) < 800
-    // ) {
-    //   //express
-    //   return (
-    //     <button key={linie.id} className="btn btn-warning badgelinie">
-    //       {linie.name}
-    //     </button>
-    //   );
-    // } else {
-    //   return (
-    //     <button key={linie.id} className="btn btn-primary badgelinie">
-    //       {linie.name}
-    //     </button>
-    //   );
-    // }
-
     return (
       <button
         key={linie.id}
